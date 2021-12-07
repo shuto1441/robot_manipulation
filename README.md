9463829286b566637c6a7fe847e9061720556856
shuto1441 <shuto1441@gmail.com>
2021-12-07 21:34:09 +0900

    update

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # robot_manipulation
 
-# Reference
+## Install
+```
+cd ~/catkin_ws/src
+git clone https://github.com/shuto1441/dobot.git
+git clone https://github.com/shuto1441/robot_manipulation.git
+cd ../
+catkin build
+```
+
+## Ubuntu20.04 
+Creating symbolic links to OS standard python commands
+
+```
+sudo apt install python-is-python3
+```
+
+## Reference
 https://github.com/shuto1441/dobot
--- scripts/Dobot_controller.py
+++ scripts/Dobot_controller.py
@@ -1,41 +1,31 @@
-#! /usr/bin/env python3
-
-import rospy
-from sensor_msgs.msg import Joy
-from geometry_msgs.msg import Twist
-# import sys
-# LIBPATH = "../../dobot/src"
-# sys.path.append(LIBPATH)
+#!/usr/bin/env python
+from __future__ import print_function
 import DobotClient as dc
+import tkinter as tk
 
-def control(msg):#joy to twist
+if __name__ == "__main__":
     isJoint = False
-    if(msg.buttons[3]):
-        dc.set_jog_cmd(isJoint, 1)
-    elif(msg.buttons[1]):
-        dc.set_jog_cmd(isJoint, 2)
-    elif(msg.buttons[0]):
-        dc.set_jog_cmd(isJoint, 3)
-    elif(msg.buttons[2]):
-        dc.set_jog_cmd(isJoint, 4)
-    elif(msg.buttons[4]):
-        dc.set_jog_cmd(isJoint, 5)
-    elif(msg.buttons[5]):
-        dc.set_jog_cmd(isJoint, 6)
-    elif(msg.buttons[6]):
-        dc.set_jog_cmd(isJoint, 7)
-    elif(msg.buttons[7]):
-        dc.set_jog_cmd(isJoint, 8)
-    elif(msg.buttons[8]):
-        dc.set_home_cmd()
-    elif(msg.buttons[9]):
-        dc.set_jog_cmd(isJoint, 0)
-    elif(msg.buttons[10]):
-        print(dc.get_pose())
-
-if __name__ == '__main__':
-    #ノードの初期化
-    rospy.init_node('controller')
-    #購読
-    rospy.Subscriber('/joy', Joy, control)
-    rospy.spin()
+    master = tk.Tk()
+    button1 = tk.Button(master, text="foward", command=lambda: dc.set_jog_cmd(isJoint, 1))
+    button1.pack()
+    button2 = tk.Button(master, text="back", command=lambda: dc.set_jog_cmd(isJoint, 2))
+    button2.pack()
+    button3 = tk.Button(master, text="left", command=lambda: dc.set_jog_cmd(isJoint, 3))
+    button3.pack()
+    button4 = tk.Button(master, text="right", command=lambda: dc.set_jog_cmd(isJoint, 4))
+    button4.pack()
+    button5 = tk.Button(master, text="up", command=lambda: dc.set_jog_cmd(isJoint, 5))
+    button5.pack()
+    button6 = tk.Button(master, text="down", command=lambda: dc.set_jog_cmd(isJoint, 6))
+    button6.pack()
+    button7 = tk.Button(master, text="left roll", command=lambda: dc.set_jog_cmd(isJoint, 7))
+    button7.pack()
+    button8 = tk.Button(master, text="right roll", command=lambda: dc.set_jog_cmd(isJoint, 8))
+    button8.pack()
+    button9 = tk.Button(master, text="stop", command=lambda: dc.set_jog_cmd(isJoint, 0))
+    button9.pack()
+    button10 = tk.Button(master, text="homing", command=lambda: dc.set_home_cmd())
+    button10.pack()
+    button11 = tk.Button(master, text="get pose", command=lambda: print(dc.get_pose()))
+    button11.pack()
+    master.mainloop()
