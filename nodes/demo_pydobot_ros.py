ea0caa04810c0f43eff3c0585335dc8a690c5623
shuto1441 <shuto1441@gmail.com>
2021-12-23 18:20:09 +0900

    demo_py

--- nodes/demo_pydobot_ros.py
+++ nodes/demo_pydobot_ros.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-from dobot_py.pydobot_ros import Dobot
+from robot_manipulation.pydobot_ros import Dobot
 
 import tkinter as tk
 
 if __name__ == "__main__":
     #dobot名の指定
-    dobot = Dobot('dobot1')
+    dobot = Dobot()
     #ホーミング
-    dobot.home()
+    #dobot.home()
     #速度設定　spped(速度，加速度)
-    dobot.speed(400, 400)
+    dobot.speed(800, 800)
     #移動(x, y, z, r)
-    dobot.move_to(200, 0, 0, 0)
-    dobot.move_to(250, 0, 0, 0)
+    dobot.move_to(200, 200, 0, 0)
+    # dobot.move_to(250, 0, 0, 0)
     #待機(ms)
-    dobot.wait(1000)
-    dobot.move_to(200, 0, 0, 0)
-    dobot.move_to(250, 0, 0, 0)
+    # dobot.wait(1000)
+    # dobot.move_to(200, 0, 0, 0)
+    # dobot.move_to(250, 0, 0, 0)
     #吸着(ON)
-    dobot.suck(1)
-    dobot.wait(3000)
+    # dobot.suck(1)
+    # dobot.wait(3000)
     #吸着(OFF)
-    dobot.suck(0)
-    print(dobot.pose())
+    # dobot.suck(0)
+    #print(dobot.pose())
